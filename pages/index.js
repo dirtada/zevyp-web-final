1c5785800ba36cba55b80f87bf3acbd4b7e94e0e
dirtada <47221293+dirtada@users.noreply.github.com>
2025-06-29 19:17:44 +0200

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center text-center">
+      <h1 className="text-4xl font-bold">Zevyp.cz – Výkopové a zemní práce</h1>
+    </div>
+  );
+}
